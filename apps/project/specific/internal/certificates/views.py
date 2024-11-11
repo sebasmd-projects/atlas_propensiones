4882d6ee7b52af7b6fc60dc64d203469cb3cbbc3
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-11-11 00:31:44 -0500

    [test] quick test in production with external img

--- apps/project/specific/internal/certificates/views.py
+++ apps/project/specific/internal/certificates/views.py
@@ -113,7 +113,7 @@ class CertificateDetailView(DetailView):
         img_qr = qr.make_image(fill="black", back_color="white").convert("RGB")
 
         # Insertar favicon en el centro
-        icon_url = "https://propensionesabogados.com/public_media/horizontal-logo.png"
+        icon_url = "https://cbh.fundacionleirion.com/static/imgs/favicon/android-chrome-512x512.png"
         response = requests.get(icon_url)
         icon = Image.open(BytesIO(response.content))
         
